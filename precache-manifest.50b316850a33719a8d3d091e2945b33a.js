62d932b28b7573a92df7af73fafc2edece682416
CodeKhushal <khushalsachdeva22@gmail.com>
2022-12-21 00:35:49 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.50b316850a33719a8d3d091e2945b33a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2284f2c08e6f6fdc8f7c",
+    "url": "/TextUtility-App/static/css/main.36c2fe02.chunk.css"
+  },
+  {
+    "revision": "2284f2c08e6f6fdc8f7c",
+    "url": "/TextUtility-App/static/js/main.2284f2c0.chunk.js"
+  },
+  {
+    "revision": "02e73066ff27f2a42269",
+    "url": "/TextUtility-App/static/js/1.02e73066.chunk.js"
+  },
+  {
+    "revision": "f3ebf9fdb0444e016e23",
+    "url": "/TextUtility-App/static/js/runtime~main.f3ebf9fd.js"
+  },
+  {
+    "revision": "67b7f075e85c9719811f4dff96889108",
+    "url": "/TextUtility-App/index.html"
+  }
+];
