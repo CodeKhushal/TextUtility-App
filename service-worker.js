03701a6b55dbecd3b2ceeb3e210a07a60ff65a5d
CodeKhushal <khushalsachdeva22@gmail.com>
2023-01-08 23:06:35 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7520d6ee9ac38cb2d57089dcdccbccc2.js"
+  "/TextUtility-App/precache-manifest.e6bfe4e434d10348a1f3a71bacdde5a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/TextUtility-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
